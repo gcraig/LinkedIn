e087556856958ac33264bbdf6f7f396aa1952543
gcraig <georgeacraig@gmail.com>
2014-04-30 16:13:47 -0500

    initial

---
+++ IntegerTest.java
@@ -0,0 +1,39 @@
+
+public class IntegerTest {
+    
+    public void run() {
+
+        Integer a = 1;
+        Integer b = new Integer(2);
+        Integer c = a + b;
+
+        isObj(a);
+        isObj(b);
+        isObj(c);
+
+        isPrim(a);
+        isPrim(b);
+        isPrim(c);
+    }
+
+    public void isObj(Integer x) {
+        if (x.getClass().equals(Integer.TYPE)) {
+            System.out.println("Obj");
+        } else {
+            System.out.println("Prim");
+        }
+    }
+
+    public void isPrim(int x) {
+        Integer i = x;
+        if (i.getClass().equals(Integer.TYPE)) {
+            System.out.println("Obj");
+        } else {
+            System.out.println("Prim");
+        }
+    }    
+
+    public static void main(String[] args) {
+        new IntegerTest().run();
+    }
+}
